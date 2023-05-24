e870bb39767899c58d452bc817a186ea063344d8
조용훈 <sjsin0905@naver.com>
2023-05-25 02:07:05 +0900

    백준 문제풀이

--- Baekjoon_Python/practice/test.py
+++ Baekjoon_Python/practice/test.py
@@ -1,7 +1,3 @@
-li = []
-for _ in range(int(input())):
-    n, d, m, y = input().split()
-    li.append([n, int(d), int(m), int(y)])
-li.sort(key=lambda x:(x[3],x[2],x[1]))
-print(li[-1][0])
-print(li[0][0])
+# 1550번 정답
+
+print(int(input(), 16))
